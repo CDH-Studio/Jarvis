632f5eccfffa09b5df18e1ddfad4a09b5e77caaf
dynamic11 <dynamic11@gmail.com>
2019-02-03 14:33:52 -0500

    added username to nav

--- app/Validators/LoginUser.js
+++ app/Validators/LoginUser.js
@@ -1,11 +1,25 @@
 'use strict'
 
-class LoginUser {
+class CreateUser {
   get rules () {
     return {
-      // validation rules
+      'email': 'required|email',
+      'password': 'required'
     }
+  }
+
+  get messages() {
+    return {
+      'required': 'Woah now, {{ field }} is required.',
+    }
+  }
+
+  async fails(error) {
+    this.ctx.session.withErrors(error)
+      .flashAll();
+    
+    return this.ctx.response.redirect('back');
   }
 }
 
-module.exports = LoginUser
+module.exports = CreateUser
