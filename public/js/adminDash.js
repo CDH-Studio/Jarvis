20393ebbabd89fdd6566e768a72114e569354e17
Peter-Lam <peter.quach.lam@gmail.com>
2019-02-05 14:24:45 -0500

    Changed " to ' to conform with coding conventions

--- public/js/adminDash.js
+++ public/js/adminDash.js
@@ -1,11 +1,11 @@
 (function($) {
-  "use strict"; // Start of use strict
+  'use strict'; // Start of use strict
 
   // Toggle the side navigation
-  $("#sidebarToggle").on('click', function(e) {
+  $('#sidebarToggle').on('click', function(e) {
     e.preventDefault();
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
+    $('body').toggleClass('sidebar-toggled');
+    $('.sidebar').toggleClass('toggled');
   });
 
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
