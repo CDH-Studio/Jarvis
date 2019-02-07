a4274f08cc8d8d3532b5ca6403bb0fd52179af1b
TunaButter <liyunwei10@gmail.com>
2019-02-07 10:01:07 -0500

    changed user role column type to int

--- database/migrations/1503248427885_user_schema.js
+++ database/migrations/1503248427885_user_schema.js
@@ -10,7 +10,7 @@ class UserSchema extends Schema {
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
-      table.string('role', 20).notNullable()
+      table.int('role').notNullable()
       table.timestamps()
     })
   }
