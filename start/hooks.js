78061e04f97ecd670b6ff8dc875c9290c4757f5d
TunaButter <liyunwei10@gmail.com>
2019-02-08 12:07:53 -0500

    validator to check fields matching

---
+++ start/hooks.js
@@ -0,0 +1,17 @@
+const { hooks } = require('@adonisjs/ignitor')
+
+hooks.after.providersBooted(() => {
+	const Validator = use('Validator');
+	const sameFn = async (data, field, message, args) => {
+		if (!data[field]) {
+			return
+		}
+
+
+		if (data[field] !== args[0]) {
+			throw message
+		}
+	}
+
+	Validator.extend('same', sameFn)
+});
