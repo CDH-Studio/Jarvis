bf9a46c0eb893b1fc4164e8842c52ba35ebd6bad
Majd <majd_khodr@hotmail.com>
2019-02-08 15:40:45 -0500

    Insertion into the database, and file uplaod work - missing naming convention for files

--- app/Controllers/Http/RoomController.js
+++ app/Controllers/Http/RoomController.js
@@ -1,13 +1,53 @@
 'use strict'
 const Room = use('App/Models/Room');
+const Helpers = use('Helpers');
 
 class RoomController {
+	// Adds a room Object into the Database
 	async addRoom({ request, response, session }) {
 		try {
+			// Retrieves user input
 			const body = request.post();
+
+			// Populates the room object's values 
 			const room = new Room();
 			room.name = body.name;
-			room.num = body.num;
+			room.location = body.location;
+			room.telephone = body.telephoneNumber;
+			room.seats = body.tableSeats;
+			room.capacity = body.maximumCapacity;
+			room.projector = body.projector;
+			room.whiteboard = body.whiteboard;
+
+			// Upload process - Floor Plan
+			const floorPlanImage = request.file('floorPlan', {
+				types: ['image'],
+				size: '2mb'
+			});
+
+			await floorPlanImage.move(Helpers.publicPath('uploads/floorPlans/'), {
+				// Name must follow specific guidlines - CANNOT HAVE THE SAME NAME 
+				name: 'testFloorPlan.png',
+			});
+
+			if (!floorPlanImage.moved()) {
+				return profilePic.error().message;
+			}
+
+			// Upload process - Room Picture
+			const roomImage = request.file('roomPicture', {
+				types: ['image'],
+				size: '2mb'
+			});
+
+			await roomImage.move(Helpers.publicPath('uploads/roomPictures/'), {
+				name: 'testRoomImage.png',
+			});
+
+			if (!roomImage.moved()) {
+				return profilePic.error().message;
+			}
+
 			await room.save();
 
 			session.flash({ notification: 'Room Added!' });
