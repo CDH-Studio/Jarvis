e4bce232dc0da6c4d35e6dab62161a46d7514d75
TunaButter <liyunwei10@gmail.com>
2019-02-15 09:00:22 -0500

    calendar column for rooms table

--- database/migrations/1549469259491_room_schema.js
+++ database/migrations/1549469259491_room_schema.js
@@ -4,7 +4,6 @@
 const Schema = use('Schema');
 
 class RoomsSchema extends Schema {
-<<<<<<< HEAD
   up() {
     this.create('rooms', (table) => {
       table.string('name', 20).notNullable().unique()
@@ -26,28 +25,6 @@ class RoomsSchema extends Schema {
       table.timestamps()
     })
   }
-=======
-	up () {
-		this.create('rooms', (table) => {
-			table.string('name', 20).notNullable().unique();
-			table.string('location', 100).notNullable();
-			table.string('telephone', 20);
-			table.integer('seats', 500).notNullable();
-			table.integer('capacity', 500).notNullable();
-			table.bool('projector');
-			table.bool('whiteboard');
-			table.bool('flipchart');
-			table.bool('audioConference');
-			table.bool('videoConference');
-			table.string('extraEquipment', 100);
-			table.string('comment', 100);
-			table.string('floorplan', 100);
-			table.string('picture', 100);
-			table.primary('name');
-			table.timestamps();
-		});
-	}
->>>>>>> dev
 
 	down () {
 		this.drop('rooms');
