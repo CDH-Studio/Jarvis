51b79ff5164708bfe2e1c46ad41ee018926ad918
TunaButter <liyunwei10@gmail.com>
2019-02-18 10:47:59 -0500

    comments for room controller

--- app/Controllers/Http/RoomController.js
+++ app/Controllers/Http/RoomController.js
@@ -4,6 +4,12 @@ const Token = use('App/Models/Token');
 const Helpers = use('Helpers');
 const graph = require('@microsoft/microsoft-graph-client');
 
+/**
+ * Retrieve access token for Microsoft Graph from the data basebase.
+ *
+ * @returns {Object} The access token.
+ *
+ */
 async function getAccessToken () {
 	try {
 		const results = await Token.findBy('type', 'access');
@@ -67,149 +73,6 @@ class RoomController {
 			return view.render('adminDash.roomDetails', { params, room });
 		} catch (err) {
 			console.log(err);
-		}
-	}
-
-	async getAllRooms ({ view }) {
-		const results = await Room.all();
-		const rooms = results.toJSON();
-
-		rooms.sort((a, b) => {
-			return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
-		});
-
-		return view.render('userPages.results', { rooms });
-	}
-
-	async goToDetails ({ request, view }) {
-		const room = request.only(['title', 'floor', 'seats', 'maxCapacity', 'phoneNumber']);
-		console.log(room);
-
-		return view.render('userPages.roomDetails', { room });
-	}
-
-	async confirmBooking ({ request, response }) {
-		const { meeting, date, from, to, room } = request.only(['meeting', 'date', 'from', 'to', 'room']);
-		const results = await Room
-			.findBy('name', room);
-		const row = results.toJSON();
-		const calendar = row.calendar;
-		console.log(calendar);
-
-		const eventInfo = {
-			'subject': meeting,
-			'body': {
-				'contentType': 'HTML',
-				'content': 'Does late morning work for you?'
-			},
-			'start': {
-				'dateTime': `${date}T${from}`,
-				'timeZone': 'Eastern Standard Time'
-			},
-			'end': {
-				'dateTime': `${date}T${to}`,
-				'timeZone': 'Eastern Standard Time'
-			},
-			'location': {
-				'displayName': room
-			},
-			'attendees': []
-		};
-
-		const createdEvent = this.createEvent(eventInfo, calendar);
-
-		if (createdEvent) { return response.redirect('/booking'); }
-	}
-
-	async getEvents () {
-		const accessToken = await getAccessToken();
-
-		if (accessToken) {
-			const client = graph.Client.init({
-				authProvider: (done) => {
-					done(null, accessToken);
-				}
-			});
-
-			try {
-				const events = await client
-					.api('/me/events')
-					.select('subject,organizer,start,end')
-					// .orderby('createdDateTime DESC')
-					.get();
-
-				return events;
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	}
-
-	async getCalendars () {
-		const accessToken = await getAccessToken();
-
-		if (accessToken) {
-			const client = graph.Client.init({
-				authProvider: (done) => {
-					done(null, accessToken);
-				}
-			});
-
-			try {
-				const calendars = await client
-					.api('/me/calendars')
-					// .orderby('createdDateTime DESC')
-					.get();
-
-				return calendars;
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	}
-
-	async getCalendar (calendarId) {
-		const accessToken = await getAccessToken();
-
-		if (accessToken) {
-			const client = graph.Client.init({
-				authProvider: (done) => {
-					done(null, accessToken);
-				}
-			});
-
-			try {
-				const calendars = await client
-					.api(`/me/calendars/${calendarId}`)
-					// .orderby('createdDateTime DESC')
-					.get();
-
-				return calendars;
-			} catch (err) {
-				console.log(err);
-			}
-		}
-	}
-
-	async createEvent (eventInfo, calendarId) {
-		const accessToken = await getAccessToken();
-
-		if (accessToken) {
-			const client = graph.Client.init({
-				authProvider: (done) => {
-					done(null, accessToken);
-				}
-			});
-
-			try {
-				const newEvent = await client
-					.api(`/me/calendars/${calendarId}/events`)
-					.post(eventInfo);
-
-				return newEvent;
-			} catch (err) {
-				console.log(err);
-			}
 		}
 	}
 
@@ -283,6 +146,187 @@ class RoomController {
 			return view.render('adminDash.roomDetails', { room: room });
 		} catch (error) {
 			return response.redirect('/');
+		}
+	}
+
+	/**
+	 * Query all the rooms from the database and render the results page.
+	 *
+	 * @param {Object} Context The context object.
+	 */
+	async getAllRooms ({ view }) {
+		const results = await Room.all();
+		const rooms = results.toJSON();
+
+		// Sort the results by name
+		rooms.sort((a, b) => {
+			return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+		});
+
+		return view.render('userPages.results', { rooms });
+	}
+
+	/**
+	 * Navigate to the details page of specified room.
+	 *
+	 * @param {Object} Context The context object.
+	 */
+	async goToDetails ({ request, view }) {
+		const room = request.only(['title', 'floor', 'seats', 'maxCapacity', 'phoneNumber']);
+		console.log(room);
+
+		return view.render('userPages.roomDetails', { room });
+	}
+
+	/**
+	 * Create the requested event on the room calendar.
+	 *
+	 * @param {Object} Context The context object.
+	 */
+	async confirmBooking ({ request, response }) {
+		const { meeting, date, from, to, room } = request.only(['meeting', 'date', 'from', 'to', 'room']);
+		const results = await Room
+			.findBy('name', room);
+		const row = results.toJSON();
+		const calendar = row.calendar;
+
+		// Information of the event
+		const eventInfo = {
+			'subject': meeting,
+			'body': {
+				'contentType': 'HTML',
+				'content': 'Does late morning work for you?'
+			},
+			'start': {
+				'dateTime': `${date}T${from}`,
+				'timeZone': 'Eastern Standard Time'
+			},
+			'end': {
+				'dateTime': `${date}T${to}`,
+				'timeZone': 'Eastern Standard Time'
+			},
+			'location': {
+				'displayName': room
+			},
+			'attendees': []
+		};
+
+		// Create the event
+		const createdEvent = this.createEvent(eventInfo, calendar);
+
+		if (createdEvent) {
+			return response.redirect('/booking');
+		}
+	}
+
+	/**
+	 * Query all events of the specified room calendar.
+	 *
+	 * @param {String} calendarId The id of the room calendar.
+	 */
+	async getEvents (calendarId) {
+		const accessToken = await getAccessToken();
+
+		if (accessToken) {
+			const client = graph.Client.init({
+				authProvider: (done) => {
+					done(null, accessToken);
+				}
+			});
+
+			try {
+				const events = await client
+					.api(`/me/calendars/${calendarId}/events`)
+					.select('subject,organizer,start,end')
+					// .orderby('createdDateTime DESC')
+					.get();
+
+				return events;
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	}
+
+	/**
+	 * Query all the room calendars.
+	 */
+	async getCalendars () {
+		const accessToken = await getAccessToken();
+
+		if (accessToken) {
+			const client = graph.Client.init({
+				authProvider: (done) => {
+					done(null, accessToken);
+				}
+			});
+
+			try {
+				const calendars = await client
+					.api('/me/calendars')
+					// .orderby('createdDateTime DESC')
+					.get();
+
+				return calendars;
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	}
+
+	/**
+	 * Query the specified room calendar.
+	 *
+	 * @param {String} calendarId The id of the room calendar.
+	 */
+	async getCalendar (calendarId) {
+		const accessToken = await getAccessToken();
+
+		if (accessToken) {
+			const client = graph.Client.init({
+				authProvider: (done) => {
+					done(null, accessToken);
+				}
+			});
+
+			try {
+				const calendars = await client
+					.api(`/me/calendars/${calendarId}`)
+					// .orderby('createdDateTime DESC')
+					.get();
+
+				return calendars;
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	}
+
+	/**
+	 * Create an event on the specified room calendar.
+	 *  
+	 * @param {*} eventInfo Information of the event.
+	 * @param {*} calendarId The id of the room calendar.
+	 */
+	async createEvent (eventInfo, calendarId) {
+		const accessToken = await getAccessToken();
+
+		if (accessToken) {
+			const client = graph.Client.init({
+				authProvider: (done) => {
+					done(null, accessToken);
+				}
+			});
+
+			try {
+				const newEvent = await client
+					.api(`/me/calendars/${calendarId}/events`)
+					.post(eventInfo);
+
+				return newEvent;
+			} catch (err) {
+				console.log(err);
+			}
 		}
 	}
 }
