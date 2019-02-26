ae639d404c3c6981469a34b717e9a0f4e8511cc9
TunaButter <liyunwei10@gmail.com>
2019-02-26 14:29:49 -0500

    no notifications for admin

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -2,20 +2,14 @@
 'use strict';
 
 class HomeController {
-	async dashboard ({ response, auth, session }) {
+	async dashboard ({ response, auth }) {
 		try {
 			await auth.check();
 			var d = new Date();
 			var date = d.toLocaleDateString();
 			if (auth.user.role === 1) {
-				session.flash({
-					notification: 'You are logged in as an Admin user.'
-				});
 				return response.route('adminDash', { auth, date });
 			} else {
-				session.flash({
-					notification: 'You are logged in as an Employee user.'
-				});
 				return response.route('booking', { auth });
 			}
 		} catch (error) {
