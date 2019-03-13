4190d7c52e8e898f77c94da9395d0be5b56f2fe4
TunaButter <liyunwei10@gmail.com>
2019-03-13 13:55:39 -0400

    wit api call

--- app/Controllers/Ws/ChatbotController.js
+++ app/Controllers/Ws/ChatbotController.js
@@ -1,13 +1,25 @@
 'use strict';
 
+const Axios = require('axios');
+
 class ChatbotController {
 	constructor ({ socket, request }) {
 		this.socket = socket;
 		this.request = request;
 	}
 
-	onMessage (message) {
+	async onMessage (message) {
 		console.log(message);
+
+		const url = 'https://api.wit.ai/message?v=20190313&q=I%20want%20to%20book%20a%20room.';
+		const headers = {
+			'Authorization': 'Bearer D2TSQTSEYR7I3S7J53S6R7FVNORMRYFO'
+		};
+
+		const result = await Axios.get(url, {
+			headers: headers
+		});
+		console.log(result.data.entities);
 
 		this.socket.broadcastToAll('message', message);
 	}
