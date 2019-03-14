9ff9fae67a99df9e1185007921d3d58aaa30a544
dynamic11 <dynamic11@gmail.com>
2019-03-14 13:08:47 -0400

    fixed logic for cancel in manage user bookings

--- start/routes.js
+++ start/routes.js
@@ -76,7 +76,7 @@ Route.post('/editReview/:id', 'RoomController.editReview').as('editReview').midd
 // Bookings
 //= ========================================================================
 Route.post('/goToDetails', 'RoomController.goToDetails').as('goToDetails').middleware(['auth']); // needs to be changed to get
-Route.get('/user/:id/bookings', 'RoomController.viewBookings').as('viewBookings').middleware(['auth']);
+Route.get('/user/:id/bookings', 'RoomController.viewUserBookings').as('viewBookings').middleware(['auth']);
 Route.get('/cancelBooking/:id', 'RoomController.cancelBooking').as('cancelBooking').middleware(['auth']);
 
 // Employee user pages
