027bdf11221b70e3a78dd795f178bdb0d95dc826
Majd <majd_khodr@hotmail.com>
2019-03-28 15:41:45 -0400

    display one graph on the adminDash

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,4 +1,5 @@
 'use strict';
+const Room = use('App/Models/Room');
 
 class HomeController {
 	/**
@@ -35,7 +36,32 @@ class HomeController {
 	*
 	*/
 	async adminDashboard ({ view }) {
-		return view.render('adminDash');
+		// Retrieve number of active rooms
+		let countActive = await Room
+			.query()
+			.where('state', 1)
+			.count();
+
+		// Retrieve number of deactive rooms
+		let countDeactive = await Room
+			.query()
+			.where('state', 2)
+			.count();
+
+		// Retrieve number of rooms under maintenance
+		let countMaint = await Room
+			.query()
+			.where('state', 3)
+			.count();
+
+		// Create statistic array with custom keys
+		var stats = {};
+		stats['total'] = countActive[0]['count(*)'] + countDeactive[0]['count(*)'] + countMaint[0]['count(*)'];
+		stats['active'] = countActive[0]['count(*)'];
+		stats['deactive'] = countDeactive[0]['count(*)'];
+		stats['maintenance'] = countMaint[0]['count(*)'];
+
+		return view.render('adminDash', { stats: stats });
 	}
 
 	/**
