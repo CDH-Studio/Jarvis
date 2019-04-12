d28938d6def12360d82caffd495ad37ca9567456
Peter-Lam <peter.quach.lam@gmail.com>
2019-04-12 15:00:53 -0400

    Changes to Book Room Form

--- public/js/roomBookingForm.js
+++ public/js/roomBookingForm.js
@@ -1,6 +1,6 @@
+
 (function ($) {
 	'use strict'; // Start of use strict
-
 	var selection = document.getElementById('recurringSelect');
 	if (selection.value === '1') {
 		document.getElementById('recurringInfo').style.display = '';
@@ -18,42 +18,22 @@
 	}
 
 	// remove the is-invalid output when the user inputs a new value
-	function removeErrorClass (idOfInput) {
-		$('#' + idOfInput).removeClass('is-invalid');
+	function removeErrorClass(idOfInput){
+		$('#'+idOfInput).removeClass('is-invalid');
 		// Ih field is the calendar, then remove styling from erorr message
-		if (idOfInput === 'date') {
+		if (idOfInput == 'date') {
 			$('#date-error').removeClass('show-invalid');
-		} else if (idOfInput === 'from') {
-			$('#from-error').removeClass('show-invalid');
-		} else if (idOfInput === 'to') {
-			$('#to-error').removeClass('show-invalid');
 		}
 	}
-	/* eslint-enable */
-	// Setting from and to times
-	const currentTime = new Date();
-	const currentHour = currentTime.getHours();
-	const currentMinutes = currentTime.getMinutes();
-	let fromTime;
-	let toTime;
-	if (currentMinutes <= 30) {
-		fromTime = currentHour + ':30';
-		toTime = currentHour + 1 + ':30';
-	} else {
-		fromTime = currentHour + 1 + ':00';
-		toTime = currentHour + 2 + ':00';
-	}
-	$('#from').datebox({
-		mode: 'timebox',
-		defaultValue: fromTime,
-		minuteStep: '30',
-		minuteStepRound: '1'
-	});
-	$('#to').datebox({
-		mode: 'timebox',
-		defaultValue: toTime,
-		minuteStep: '30',
-		minuteStepRound: '1'
+
+
+	// update from and to inputs from dropdown
+	$(document).on("click",".dropdown-menu a", function(ev) {
+		$(this).siblings().removeClass('active');
+		$(this).addClass("active");
+		$(this).parents(".timePicker").find('.time-form').val($(this).attr('data-value'));
+		$(this).parents(".timePicker").find('.time-form').removeClass('is-invalid');
+		$(this).parents(".timePicker").siblings('.invalid-feedback').removeClass('show-invalid');
 	});
 
 })(jQuery); // eslint-disable-line
