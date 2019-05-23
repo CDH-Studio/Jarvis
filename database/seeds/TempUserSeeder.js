c61218b30caef6dd3841405a7c3f160b010cc87e
TunaButter <liyunwei10@gmail.com>
2019-05-23 16:24:12 -0400

    annie

--- database/seeds/TempUserSeeder.js
+++ database/seeds/TempUserSeeder.js
@@ -20,7 +20,8 @@ class TempUserSeeder {
 			{ firstname: 'Yunwei', lastname: 'User', email: 'ylithrowaway@gmail.com', password: '123', floor: '8', tower: '2', role_id: '2', verified: 1 },
 			{ firstname: 'Ali', lastname: 'Admin', email: 'admin@gmail.com', password: '123456', floor: '5', tower: '2', role_id: '1', verified: 1 },
 			{ firstname: 'Ali', lastname: 'User', email: 'user@gmail.com', password: '123456', floor: '2', tower: '2', role_id: '2', verified: 1 },
-			{ firstname: 'Mena', lastname: 'Machado', email: 'mena.machado@canada.ca', password: '123', floor: '3', tower: '2', role_id: '2', verified: 1 }
+			{ firstname: 'Mena', lastname: 'Machado', email: 'mena.machado@canada.ca', password: '123', floor: '3', tower: '2', role_id: '2', verified: 1 },
+			{ firstname: 'Annie', lastname: 'Belanger', email: 'annie.belanger2@canada.ca', password: '123', floor: '3', tower: '2', role_id: '2', verified: 1 }
 		];
 
 		var count = await Users.getCount();
