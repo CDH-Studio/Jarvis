466611141699ccd0d17c910301da8f806bc09d9b
TunaButter <liyunwei10@gmail.com>
2019-06-11 12:30:11 -0400

    userdash bookings french

--- public/js/notify.js
+++ public/js/notify.js
@@ -1,74 +1,74 @@
-// export function notify ({
-// 	$,
-// 	message,
-// 	type = 'success',
-// 	x = 10,
-// 	y = 10,
-// 	from = 'top',
-// 	align = 'right',
-// 	url = '',
-// 	enter = 'fadeInRight',
-// 	exit = 'fadeOutUp',
-// 	element = 'body',
-// 	delay = 5000
-// } = {}) {
-// 	$.notify({
-// 		message: message,
-// 		url: url
-// 	}, {
-// 		element: element,
-// 		type: type,
-// 		animate: {
-// 			enter: 'animated ' + enter,
-// 			exit: 'animated ' + exit
-// 		},
-// 		placement: {
-// 			from: from,
-// 			align: align
-// 		},
-// 		offset: {
-// 			x: x,
-// 			y: y
-// 		},
-// 		spacing: 10,
-// 		z_index: 1031,
-// 		delay: delay
-// 	});
-// };
-
-export function notify (
+export function notify ({
 	$,
 	message,
-	type,
-	x,
-	y,
-	from,
-	align,
-	url,
-	enter,
-	exit,
-	element,
-	delay) {
+	type = 'success',
+	x = 10,
+	y = 10,
+	from = 'top',
+	align = 'right',
+	url = '',
+	enter = 'fadeInRight',
+	exit = 'fadeOutUp',
+	element = 'body',
+	delay = 5000
+} = {}) {
 	$.notify({
 		message: message,
-		url: url ? url : ''
+		url: url
 	}, {
-		element: element ? element : 'body',
-		type: type ? type : 'success',
+		element: element,
+		type: type,
 		animate: {
-			enter: 'animated ' + enter ? enter : 'fadeInRight',
-			exit: 'animated ' + exit ? exit : 'fadeOutUp'
+			enter: 'animated ' + enter,
+			exit: 'animated ' + exit
 		},
 		placement: {
-			from: from ? from : 'top',
-			align: align ? align : 'right'
+			from: from,
+			align: align
 		},
 		offset: {
-			x: x ? x : 10,
-			y: y ? y : 10
+			x: x,
+			y: y
 		},
 		spacing: 10,
 		z_index: 1031,
-		delay: delay ? delay : 5000
+		delay: delay
 	});
 };
+
+// export function notify (
+// 	$,
+// 	message,
+// 	type,
+// 	x,
+// 	y,
+// 	from,
+// 	align,
+// 	url,
+// 	enter,
+// 	exit,
+// 	element,
+// 	delay) {
+// 	$.notify({
+// 		message: message,
+// 		url: url ? url : ''
+// 	}, {
+// 		element: element ? element : 'body',
+// 		type: type ? type : 'success',
+// 		animate: {
+// 			enter: 'animated ' + enter ? enter : 'fadeInRight',
+// 			exit: 'animated ' + exit ? exit : 'fadeOutUp'
+// 		},
+// 		placement: {
+// 			from: from ? from : 'top',
+// 			align: align ? align : 'right'
+// 		},
+// 		offset: {
+// 			x: x ? x : 10,
+// 			y: y ? y : 10
+// 		},
+// 		spacing: 10,
+// 		z_index: 1031,
+// 		delay: delay ? delay : 5000
+// 	});
+// };
