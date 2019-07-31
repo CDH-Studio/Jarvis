a8295634ceda50e18a12630d6634645bf6b80d72
TunaButter <liyunwei10@gmail.com>
2019-07-31 16:06:11 -0400

    temp timepicker logic

--- public/js/timepicker.js
+++ public/js/timepicker.js
@@ -28,3 +28,28 @@ $(function () {
         defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
     });
 });
+
+$(function () {
+    $('#from').datetimepicker({
+        format: 'HH:mm',
+        stepping: 30,
+        defaultDate: moment(round(moment()), 'HH:mm')
+    });
+
+    $('#from').datetimepicker({
+        format: 'HH:mm',
+        stepping: 30,
+        defaultDate: moment(round(moment()), 'HH:mm')
+    });
+
+    $('#from').on("change.datetimepicker", function (e) {
+        if($('#to').datetimepicker('date').isSameOrBefore(e.date))
+            $('#to').datetimepicker('date', e.date.add(0.5, 'hour').format('HH:mm'));
+    });
+
+    $('#to').datetimepicker({
+        format: 'HH:mm',
+        stepping: 30,
+        defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
+    });
+});
