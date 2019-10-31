5418ed5cf77bd84ac2cf82b7efe4d6c8509d2296
Ali Nouri <dynamic11@users.noreply.github.com>
2019-10-31 14:11:26 -0400

    [fixed] made search forms accessible and cleaned up search (#318)
    - made search form accessible
    - rewrote and cleaned up search logic
    - restyled search result pages for both fixed and flex search
    - added additional french translations

--- app/Validators/SearchFlexible.js
+++ app/Validators/SearchFlexible.js
@@ -1,9 +1,10 @@
 /**
- * File Name: SearchRoom.js
- * Description: Validator used to validate input fields Search Room Page (Employee)
+ * File Name: SearchFlexible.js
+ * Description: Validator used to validate input fields for Flexible Search Room form (Employee)
  * Instructions: Use this validator by adding ".validator('SearchRoom')" to your route in /routes.js
  **/
 'use strict';
+const moment = require('moment');
 
 class SearchFlexible {
 	// Validate and return all fields
@@ -14,12 +15,10 @@ class SearchFlexible {
 	// Validation rules
 	get rules () {
 		// getting the current date subract 1 day
-		let afterDate = new Date();
-		afterDate.setDate(afterDate.getDate() - 1);
+		let minDate = moment().subtract(1, 'days').format();
+
 		//  getting the current date plus 3 months
-		let beforeDate = new Date();
-		beforeDate.setDate(beforeDate.getDate() + 1);
-		beforeDate.setMonth(beforeDate.getMonth() + 3);
+		let maxDate = moment().add(1, 'days').add(3, 'months').format();
 
 		// Validation rules
 		return {
@@ -31,18 +30,18 @@ class SearchFlexible {
 			 * date: checks if the input field is a valid date
 			 * required: require field, cannot search without a date entered
 			 */
-			date: `required|date|after:${afterDate}|before:${beforeDate}`,
+			flexibleSearchDate: `required|date|after:${minDate}|before:${maxDate}`,
 			/**
 			 * From and To validation rules
 			 *
-			 * isAfter: the 'to' field must occur after the 'from' field (custom validator see CustomValidationProvidor.js for more)
 			 * isAfterToday: 'from' and 'to' fields must occur after the current time if the date field is the current date
-			 * isWithinRange: 'to' field cannot be more than X hours after 'from'
 			 * required: require field, cannot search without a date entered
 			 */
-			from: 'required|isAfterToday:date',
-			to: 'required|isAfter:from|isAfterToday:date',
-			duration: 'required|range:0,24'
+			flexibleSearchFrom: 'required|isAfterToday:flexibleSearchDate',
+			flexibleSearchTo: 'required|isAfter:flexibleSearchFrom|isAfterToday:flexibleSearchDate',
+			flexibleSearchSeats: 'integer',
+			flexibleSearchCapacity: 'integer',
+			flexibleSearchDuration: 'required|range:0,24'
 
 		};
 	}
@@ -50,11 +49,11 @@ class SearchFlexible {
 	// Custom error messages
 	get messages () {
 		return {
-			'date.after': 'Please enter a time in the future',
-			'date.before': 'You can only book rooms up to 3 months ahead of time',
-			'date.date': 'Please enter a valid date',
-			'from.isAfterToday': 'This field must occur after the current time',
-			'to.isAfterToday': 'This field must occur after the current time',
+			'flexibleSearchFrom.isAfterToday': 'This field must occur after the current time',
+			'flexibleSearchTo.isAfter()': 'End time must be after Start time',
+			'flexibleSearchDate.after': 'Please enter a time in the future',
+			'flexibleSearchDate.before': 'You can only book rooms up to 3 months ahead of time',
+			'flexibleSearchDate.date': 'Please enter a valid date',
 			'required': 'This field is required'
 		};
 	}
--- public/js/timepicker.js
+++ public/js/timepicker.js
@@ -24,35 +24,35 @@ $(function () {
 
 
 
-    $('#fixed-from').datetimepicker({
+    $('#fixed-search-from').datetimepicker({
         format: 'HH:mm',
         stepping: 30,
         defaultDate: moment(round(moment()), 'HH:mm')
     });
 
-    $('#fixed-from').on("change.datetimepicker", function (e) {
-        if($('#fixed-to').datetimepicker('date').isSameOrBefore(e.date))
-            $('#fixed-to').datetimepicker('date', e.date.add(0.5, 'hour').format('HH:mm'));
+    $('#fixed-search-from').on("change.datetimepicker", function (e) {
+        if($('#fixed-search-to').datetimepicker('date').isSameOrBefore(e.date))
+            $('#fixed-search-to').datetimepicker('date', e.date.add(0.5, 'hour').format('HH:mm'));
     });
 
-    $('#fixed-to').datetimepicker({
+    $('#fixed-search-to').datetimepicker({
         format: 'HH:mm',
         stepping: 30,
         defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
     });
 
-    $('#flexible-from').datetimepicker({
+    $('#flexible-search-from').datetimepicker({
         format: 'HH:mm',
         stepping: 30,
         defaultDate: moment(round(moment()), 'HH:mm')
     });
 
-    $('#flexible-from').on("change.datetimepicker", function (e) {
-        if($('#flexible-to').datetimepicker('date').isSameOrBefore(e.date))
-            $('#flexible-to').datetimepicker('date', e.date.add(0.5, 'hour').format('HH:mm'));
+    $('#flexible-search-from').on("change.datetimepicker", function (e) {
+        if($('#flexible-search-to').datetimepicker('date').isSameOrBefore(e.date))
+            $('#flexible-search-to').datetimepicker('date', e.date.add(0.5, 'hour').format('HH:mm'));
     });
 
-    $('#flexible-to').datetimepicker({
+    $('#flexible-search-to').datetimepicker({
         format: 'HH:mm',
         stepping: 30,
         defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
