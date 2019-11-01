29c793a7956ccaccda2a9d06091eb424612c988b
Ali Nouri <dynamic11@gmail.com>
2019-11-01 14:27:46 -0400

    [new] added french translated caledar

--- public/js/timepicker.js
+++ public/js/timepicker.js
@@ -22,8 +22,6 @@ $(function () {
         defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
     });
 
-
-
     $('#fixed-search-from').datetimepicker({
         format: 'HH:mm',
         stepping: 30,
@@ -58,28 +56,3 @@ $(function () {
         defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
     });
 });
-
-// $(function () {
-//     $('#from').datetimepicker({
-//         format: 'HH:mm',
-//         stepping: 30,
-//         defaultDate: moment(round(moment()), 'HH:mm')
-//     });
-
-//     $('#from').datetimepicker({
-//         format: 'HH:mm',
-//         stepping: 30,
-//         defaultDate: moment(round(moment()), 'HH:mm')
-//     });
-
-//     $('#from').on("change.datetimepicker", function (e) {
-//         if($('#to').datetimepicker('date').isSameOrBefore(e.date))
-//             $('#to').datetimepicker('date', e.date.add(0.5, 'hour').format('HH:mm'));
-//     });
-
-//     $('#to').datetimepicker({
-//         format: 'HH:mm',
-//         stepping: 30,
-//         defaultDate: moment(round(moment()), 'HH:mm').add(0.5, 'h')
-//     });
-// });
