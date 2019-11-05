a17636ca70c68dc9266a1c426f66d411e9b0cd37
yunwei.li@canada.ca <yunwei.li@ised-isde.gc.ca>
2019-11-05 12:47:28 -0500

    validator for verification 2

---
+++ app/Middleware/CheckVerified.js
@@ -0,0 +1,21 @@
+'use strict'
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+class CheckVerified {
+  /**
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Function} next
+   */
+  async handle ({ auth, response }, next) {
+		if (!auth.user.verified) {
+			return response.redirect('/');
+		}
+
+		await next();
+	}
+}
+
+module.exports = CheckVerified
