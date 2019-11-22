01eb5622e2e909275e2aa531b2fe3c59b85439e0
yunwei.li@canada.ca <yunwei.li@ised-isde.gc.ca>
2019-11-22 10:36:14 -0500

    floor name change

--- database/seeds/FloorSeeder.js
+++ database/seeds/FloorSeeder.js
@@ -15,7 +15,7 @@ const Floor = use('App/Models/Floor');
 
 class FloorSeeder {
 	async run () {
-		var floorFiller = ['Lobby', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
+		var floorFiller = ['C3', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
 		var buildingFiller = ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'];
 		var count = await Floor.getCount();
 
