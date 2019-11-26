b9021107f3ab3c2b8a17034c62ba3d5a00412251
Yunwei Li <liyunwei10@gmail.com>
2019-11-26 13:11:32 -0500

    [fixed] search records query and pusher key update (#350)

--- app/Controllers/Http/SearchRecordController.js
+++ app/Controllers/Http/SearchRecordController.js
@@ -4,9 +4,12 @@ const moment = require('moment');
 
 class SearchRecordController {
 	async viewSearchRecords ({ view }) {
+		const start = moment().subtract(180, 'days').endOf('day').format('YYYY-MM-DD');
+
 		const records = (await SearchRecord
 			.query()
 			.with('user')
+			.where('created_at', '>', start)
 			.fetch()).toJSON();
 
 		return view.render('adminPages.viewSearchRecords', {
