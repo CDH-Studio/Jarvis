9daf1a86338e6cbe8b8c9002e161c52d777f5ded
Ali Nouri <dynamic11@gmail.com>
2020-01-21 17:22:10 -0500

    got room seeder working

--- database/seeds/4_RoomSeeder.js
+++ database/seeds/4_RoomSeeder.js
@@ -11,28 +11,41 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-const Floor = use('App/Models/Room');
+const Room = use('App/Models/Room');
 
 class RoomSeeder {
 	async run () {
-		// load in dummy data for floor
+		// load in dummy data for rooms
 		let roomFiller = require('../seederData/rooms.json');
 
-		// count floors in database
+		// count rooms in database
 		var count = await Room.getCount();
 
 		if (count == 0) {
 			for (var i = 0; i < roomFiller.length; i++) {
-				const floor = new Floor();
-				floor.id = i === 0 ? '0' : i;
-				floor.name_english = roomFiller[i]['name_english'];
-				floor.name_french = roomFiller[i]['name_french'];
-				floor.building_id = roomFiller[i]['building_id'];
-				await floor.save();
+				const room = new Room();
+				room.name = roomFiller[i]['name'];
+				room.fullName = roomFiller[i]['fullName'];
+				room.building_id = roomFiller[i]['building_id'];
+				room.tower_id = roomFiller[i]['tower_id'];
+				room.floor_id = roomFiller[i]['floor_id'];
+				room.telephone = roomFiller[i]['telephone'];
+				room.capacity = roomFiller[i]['capacity'];
+				room.seats = roomFiller[i]['seats'];
+				room.avg_rating = roomFiller[i]['avg_rating'];
+				room.extraEquipment = roomFiller[i]['extraEquipment'];
+				room.comment = roomFiller[i]['comment'];
+				room.picture = roomFiller[i]['picture'];
+				room.picture_small = roomFiller[i]['picture_small'];
+				room.floorplan = roomFiller[i]['floorplan'];
+				room.floorplan_small = roomFiller[i]['floorplan_small'];
+				room.calendar = roomFiller[i]['calendar'];
+				room.state_id = roomFiller[i]['state_id'];
+				await room.save();
 			}
-			console.log('Floor Name DB: Finished Seeding');
+			console.log('Rooms DB: Finished Seeding');
 		} else {
-			console.log('Floor Name DB: Already Seeded');
+			console.log('Floors DB: Already Seeded');
 		}
 	}
 }
