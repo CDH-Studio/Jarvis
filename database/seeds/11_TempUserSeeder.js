c8ea4aab731386ae2032967489ffb785ee79fb1d
Ali Nouri <dynamic11@gmail.com>
2020-01-28 15:05:32 -0500

    Removed unused variable

--- database/seeds/11_TempUserSeeder.js
+++ database/seeds/11_TempUserSeeder.js
@@ -8,8 +8,6 @@
 | Seed table with user accounts for dev purposes
 |
 */
-const Users = use('App/Models/User');
-
 class TempUserSeeder {
 	async run () {
 		console.log('Users DB: No seeding required. Will use AD.');
